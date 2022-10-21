a55239d5f4a3e3f0df68caeef1d6bba3f724d843
João Augusto Druzian <johhdq@hotmail.com>
2022-10-21 14:19:50 -0300

    Alterações

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+// Exportar o objeto com as palavras do jogo como não temos integração com o BD ainda
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
