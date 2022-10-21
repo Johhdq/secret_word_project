d40386bef562ff74fbaa3b9dfe07a7c4f5017e23
João Augusto Druzian <johhdq@hotmail.com>
2022-10-21 17:05:35 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import StartScreen from './components/StartScreen/StartScreen';
+import "./App.css";
+
+// components
+import StartScreen from "./components/StartScreen/StartScreen";
+
+// react
+import { userCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <StartScreen />
+      { gameStage === "start" && <StartScreen />}
     </div>
   );
 }
